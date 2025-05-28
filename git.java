77df18c756efa40731168faf373c08f428c59f7a
Ghofrane <BenSoltane.Ghofrane@esprit.tn>
2025-05-28 11:25:44 +0200

    regler Conflit 3

--- git.java
+++ git.java
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 System.out.println('hello klein');
 System.out.println('hello mickael');
 
-=======
 System.out.println('hello world');
 
 // Commentaire pour ne rien dire 
 // je ne connais pas java 😂
->>>>>>> d4428014eb508b3dcc69353fc8da9da95270d31b
+System.out.println('Test Rebase');
